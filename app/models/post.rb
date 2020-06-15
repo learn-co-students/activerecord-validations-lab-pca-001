1912392c1e25dd837ecc8dc192c99618232fecbe
Andrew Kloecker <lt.kvothe@gmail.com>
2020-06-15 13:15:20 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
+class MatchValidator < ActiveModel::Validator
+  def validate(record)
+    puts record.title
+    unless !!record.title && record.title.match?(/Won't Believe|Secret|Top [0-9]+|Guess/)
+      record.errors[:title] << "Not clickbait"
+    end
+  end
+end
+
 class Post < ActiveRecord::Base
+
+  include ActiveModel::Validations
+
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]  }
+  validates_with MatchValidator
 end
